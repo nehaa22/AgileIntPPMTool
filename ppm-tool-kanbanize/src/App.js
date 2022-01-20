57d3608ba4bd2aeadfd8414c3e402f208482c948
nehaa22 <nehapalekar026@gmail.com>
2022-01-20 12:58:16 +0530

    added store for redux

--- ppm-tool-kanbanize/src/App.js
+++ ppm-tool-kanbanize/src/App.js
@@ -4,18 +4,22 @@ import Header from "./components/Layout/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter, Router, Route, Routes } from "react-router-dom";
 import AddProject from "./components/Project/AddProject";
+import { Provider } from "react-redux";
+import store from "./store";
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Header />
-        <Routes>
-          <Route exact path="/dashboard" element={<Dashboard />} />
-          <Route exact path="/addProject" element={<AddProject />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <div className="App">
+          <Header />
+          <Routes>
+            <Route exact path="/dashboard" element={<Dashboard />} />
+            <Route exact path="/addProject" element={<AddProject />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
